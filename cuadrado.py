a87f13c3086e85fcaa424987eb7cccb941149e34
Alejandro Ballesteros <alexbalmar@gmail.com>
2015-11-17 16:39:42 +0100

    Comentando Funciones
    Este commit es para comentar las funciones

--- cuadrado.py
+++ cuadrado.py
@@ -1,7 +1,13 @@
 class cuadrado:
 	def __init__(self, x, y):
+	"""Esta función define el cuadrado, lado x, lado y"""
 		self.x = x
 		self.y = y
 
 	def area(self):
+	"""Esta función define el área del cuadrado ladox * ladoy"""
 		return self.x * self.y
+
+	def perimeter(self, x, y):
+	"""Esta función calcula el perímetro del cuadrado, la suma de 2 veces cada lado"""
+		return (2 * self.x) + (2 * self.y)
